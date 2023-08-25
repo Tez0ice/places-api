c05d7d1733e8a164159702ec26eab8fe0b889d7c
Zayyad <muhdzayyad91@gmail.com>
2023-08-25 10:38:24 +0800

    fixing reviews

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -6,32 +6,56 @@ use App\Models\User;
 use App\Models\Place;
 use App\Models\Review;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class ReviewController extends Controller
 {
      // Create
      public function store(Request $request, $id){
-          $review = new Review();
-          $review->rating = $request->rating; 
-          $review->comments = $request->comments;
+          // $review = new Review();
+          // $review->rating = $request->rating; 
+          // $review->comments = $request->comments;
+          // $review->user_id = User::find($request->user_id)->id;
+          // $review->place_id = Place::find($id)->id;
 
+          try{
+               $place = Place::find($id);
+               $place["avg_rating"] = ($place["avg_rating"] * count($place["avg_rating"]) + $request->rating) / (count($place["avg_rating"]+1));
      
-          $review->user_id = User::find($request->user_id)->id;
-          $review->place_id = Place::find($id)->id;
+               $user = JWTAuth::parseToken()->authneticate();
+               $userId = $user->id;
+               
+               //with authenticate token we can get user Id.
 
-          if ($review->save()){
-               return response()->json([
-                    "success"=>true,    
-                    "message"=>"Place Successfully added"
-                ]);
+               Review::create([
+                    "rating"=>$request->rating,
+                    "user_id"=>$userId,
+                    "place_id"=>$id,
+                    "comments"=>$request->comments,
+               ]);
+
+               response()->json(['success'=>true,"message"=>"Review successfully added"]);
           }
 
-          else{
-               return response()->json([
-                    "success"=>false,
-                    "message"=>"Review Successfully added"
-                ]);
+          catch (\Exception $err){
+               response()->json(["sucess"=>false,"message"=>$err]);
           }
+
+
+          // if ($review->save()){
+          //      return response()->json([
+          //           "success"=>true,    
+          //           "message"=>"Place Successfully added"
+          //       ]);
+          // }
+
+          // else{
+          //      return response()->json([
+          //           "success"=>false,
+          //           "message"=>"Review Successfully added"
+          //       ]);
+          // }
+
      }
  
      // Read all
