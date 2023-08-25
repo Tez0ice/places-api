9d0dbc93425d5f7173b5d180ed3a5f4863cf57db
Zayyad <muhdzayyad91@gmail.com>
2023-08-25 11:14:26 +0800

    fixing reviews

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -20,7 +20,7 @@ class ReviewController extends Controller
 
           try{
                $place = Place::find($id);
-               $place->avg_rating = ($place->avg_rating * count($place->reviews) + $request->avg_rating) / (count($place->reviews+1));
+               $place->avg_rating = ($place->avg_rating * count($place->reviews) + $request->rating) / (count($place->reviews+1));
      
                $user = JWTAuth::parseToken()->authneticate();
                $userId = $user->id;
